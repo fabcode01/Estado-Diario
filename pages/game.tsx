0f0a14f41a11bab4ae743ee66b8ef750ff6e7b58
Fabrício Silva <fabricio85silva@hotmail.com>
2024-07-15 12:26:52 -0300

    nova pagina menu

---
+++ pages/game.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react'
+import Estados from '../data/estados'
+import EstadoModel from '@/modelos/EstadoModel'
+
+
+export function getStaticProps(){
+      // Transforma o objeto em array  
+      const estadosEmArray = Object.values(Estados)
+
+
+      // Escolhe um estado aleatorio
+      const estadoAleatorio = estadosEmArray[Math.floor(Math.random() * estadosEmArray.length)]
+
+
+      return {
+        // Pegar um estado aleatorio todo dia 
+        revalidate: 300,
+        props: {
+          estadoEscolhido: estadoAleatorio
+        }
+      }
+
+}
+
+
+
+export default function Game(props: any) {
+
+    const[nomeDoEstado, setNomeDoEstado] = useState<EstadoModel>(new EstadoModel(props.estadoEscolhido, false))
+
+    const[inputResposta, setInputResposta] = useState<string>()
+
+
+    function respostaFornecida(e: any){
+      e.preventDefault()
+
+      if(inputResposta == nomeDoEstado.NomeDoEstadoGET){
+          setNomeDoEstado(nomeDoEstado.RespostaCerta())
+      }
+    }
+
+
+
+  return (
+    <>
+      <p>{nomeDoEstado.NomeDoEstadoGET}</p>
+      
+
+      <form className='bg-gray-300' onSubmit={respostaFornecida}>
+
+          <input type="text" className='border-2' onChange={e => setInputResposta(e.target.value)}/>
+
+          <input type="submit" value="responder" />
+      </form>
+
+
+      <div
+      className='bg-green'
+      >{nomeDoEstado.acertou ? <p>Acertou</p> : ''}</div>
+
+      
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,64 +1,15 @@
-import { useEffect, useState } from 'react'
-import Estados from '../data/estados'
-import EstadoModel from '@/modelos/EstadoModel'
+import Link from "next/link";
 
 
-export function getStaticProps(){
-      // Transforma o objeto em array  
-      const estadosEmArray = Object.values(Estados)
+export default function Home() {
+    return (
+        <div>
+        <h1>Estado Diário</h1>
+        <Link href={'/game'}>
+            <button>Começar</button>
+        </Link>
+        </div>
 
-
-      // Escolhe um estado aleatorio
-      const estadoAleatorio = estadosEmArray[Math.floor(Math.random() * estadosEmArray.length)]
-
-
-      return {
-        // Pegar um estado aleatorio todo dia 
-        revalidate: 300,
-        props: {
-          estadoEscolhido: estadoAleatorio
-        }
-      }
-
-}
-
-
-
-export default function Home(props: any) {
-
-    const[nomeDoEstado, setNomeDoEstado] = useState<EstadoModel>(new EstadoModel(props.estadoEscolhido, false))
-
-    const[inputResposta, setInputResposta] = useState<string>()
-
-
-    function respostaFornecida(e: any){
-      e.preventDefault()
-
-      if(inputResposta == nomeDoEstado.NomeDoEstadoGET){
-          setNomeDoEstado(nomeDoEstado.RespostaCerta())
-      }
-    }
-
-
-
-  return (
-    <>
-      <p>{nomeDoEstado.NomeDoEstadoGET}</p>
-      
-
-      <form className='bg-gray-300' onSubmit={respostaFornecida}>
-
-          <input type="text" className='border-2' onChange={e => setInputResposta(e.target.value)}/>
-
-          <input type="submit" value="responder" />
-      </form>
-
-
-      <div
-      className='bg-green'
-      >{nomeDoEstado.acertou ? <p>Acertou</p> : ''}</div>
-
-      
-    </>
-  );
+    )
+  
 }
