31b0a65280ba938ddb8cda256f2ee6fb00c7a852
Fabrício Silva <fabricio85silva@hotmail.com>
2024-07-19 20:36:28 -0300

    fix posicao do input

--- componentes/Input.tsx
+++ componentes/Input.tsx
@@ -48,71 +48,66 @@ export default function Input(props: InputProps){
 
 
     return (
-        <div className="flex flex-col items-center w-3/4 m-auto">
-
-        <div className="
-        relative
-        top-5
-        overflow-hidden
-        bg-white
-        h-36 w-full
-        border-[1px]
-         rounded-[14px]
-        border-black
-        p-2
-        pb-5
-        font-light
-
-        dark:bg-slate-600
-        dark:text-slate-300
-        dark:border-white
-        
-        ">
-                <div className="overflow-auto h-full">
-                    {sugestao && sugestao.map((s)=>(
-                        <p className="
-                        cursor-pointer
-                        hover:bg-slate-200
-                        dark:hover:bg-slate-800
-                        
-                        
-                        ">{s}</p>
-                    ))}
-                    
-                </div>
-           </div>
-            
-
-            <form onSubmit={ResponderPalpite} className="
-             
-            
-             flex
-             items-center
-             justify-between
-             border-3
-             border-black
-             bg-white
-             w-full
-             h-[65px]
+        <div className="fixed bottom-5 w-full">
+            <div className="flex flex-col items-center w-3/4 m-auto">
+            <div className="
+            relative
+            top-5
+            overflow-hidden
+            bg-white
+            h-36 w-full
+            border-[1px]
              rounded-[14px]
-             border-[1px]
-             z-30
-
-             dark:bg-slate-600
-            dark:text-white
+            border-black
+            p-2
+            pb-5
+            font-light
+            dark:bg-slate-600
+            dark:text-slate-300
             dark:border-white
-             
+            
             ">
-               
-
-                <input type="text" value={estadoInput} onChange={e=> setEstadoInput(e.target.value)} className="outline-none p-3 rounded-[14px] bg-transparent placeholder:text-slate-300" placeholder="Escolha um estado"/>
-
-                <button className="w-8 mr-2">{enviarPalpiteIcon}</button>
-                
-                
-            </form>
-
-           
+                    <div className="overflow-auto h-full">
+                        {sugestao && sugestao.map((s)=>(
+                            <p className="
+                            cursor-pointer
+                            hover:bg-slate-200
+                            dark:hover:bg-slate-800
+            
+            
+                            ">{s}</p>
+                        ))}
+            
+                    </div>
+               </div>
+            
+                <form onSubmit={ResponderPalpite} className="
+            
+            
+                 flex
+                 items-center
+                 justify-between
+                 border-3
+                 border-black
+                 bg-white
+                 w-full
+                 h-[65px]
+                 rounded-[14px]
+                 border-[1px]
+                 z-30
+                 dark:bg-slate-600
+                dark:text-white
+                dark:border-white
+            
+                ">
+            
+                    <input type="text" value={estadoInput} onChange={e=> setEstadoInput(e.target.value)} className="outline-none p-3 rounded-[14px] bg-transparent placeholder:text-slate-300" placeholder="Escolha um estado"/>
+                    <button className="w-8 mr-2">{enviarPalpiteIcon}</button>
+            
+            
+                </form>
+            
+            </div>
         </div>
     )
 }
